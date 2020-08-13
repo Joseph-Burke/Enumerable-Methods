375f5e22f42c11f07a9b7ca00d13af29f71838fd
Joseph Burke <joseph_burke@outlook.com>
2020-08-13 15:46:17 +0100

    Add tests for #each and #each_with_index

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -13,9 +13,22 @@ describe Enumerable do
         end
     end
 
+    describe '#my_each_with_index' do
+        it 'passes each element and its index through a code block' do
+            hash = Hash.new
+            %w[apple banana cherry].my_each_with_index {|e, i| hash[i] = e}
+            expect(hash).to eql({0 => 'apple', 1 => 'banana', 2 => 'cherry'})
+        end
+    end
+
+    describe '#each' do
+        it 'passes each element through a code block' do
+            array = Array.new
+            %w[apple banana cherry].my_each {|e| array.push(e)}
+            expect(array).to eql(['apple', 'banana', 'cherry'])
+        end
+    end
 end
-
-
 
 
 
