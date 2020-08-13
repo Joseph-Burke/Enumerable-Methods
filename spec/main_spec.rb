b73b9bff625734fe7bc338a82d990fd0b6dc083c
Joseph Burke <joseph_burke@outlook.com>
2020-08-13 15:51:17 +0100

    Add tests for #my_map and #my_inject

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -21,13 +21,26 @@ describe Enumerable do
         end
     end
 
-    describe '#each' do
+    describe '#my_each' do
         it 'passes each element through a code block' do
             array = Array.new
             %w[apple banana cherry].my_each {|e| array.push(e)}
             expect(array).to eql(['apple', 'banana', 'cherry'])
         end
     end
+
+    describe '#my_map' do
+        it 'passes a code block to each element and returns a new array containing the results' do
+            expect([1, 2, 3, 4].my_map {|e| e * 2 }).to eql([2, 4, 6, 8])
+        end
+    end
+
+    describe '#my_inject' do
+        it 'returns an accumulated value' do
+            expect([1, 2, 3, 4].my_inject(:+)).to eql(10)
+        end
+    end
+
 end
 
 
